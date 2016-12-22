7c9022a8a23b44b4c91e40f861254fac27b8cd43
lilkr <maintiendrai@gmail.com>
2016-12-22 14:28:36 +0800

    modify webpack base config

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -17,17 +17,17 @@ module.exports = {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue',
+        loader: 'vue-loader',
         options: vueConfig
       },
       {
         test: /\.js$/,
-        loader: 'babel',
+        loader: 'babel-loader',
         exclude: /node_modules/
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: 'url',
+        loader: 'url-loader',
         options: {
           limit: 10000,
           name: '[name].[ext]?[hash]'
